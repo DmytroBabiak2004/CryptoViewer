d59ad2d1f2fcd7345212c018ad7aabf6a5f920ad
dmytro-babiak <dmytro.babiak.pz.2022@lpnu.ua>
2026-06-27 12:06:30 +0300

    connect coin gecko api

---
+++ WpfApp1/Models/Coin.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json.Serialization;
+
+namespace CryptoViewer.Models;
+
+public class Coin
+{
+    [JsonPropertyName("id")]
+    public string Id { get; set; }
+
+    [JsonPropertyName("symbol")]
+    public string Symbol { get; set; }
+
+    [JsonPropertyName("name")]
+    public string Name { get; set; }
+
+    [JsonPropertyName("current_price")]
+    public decimal CurrentPrice { get; set; }
+
+    [JsonPropertyName("price_change_percentage_24h")]
+    public decimal PriceChange24h { get; set; }
+
+    [JsonPropertyName("market_cap")]
+    public long MarketCap { get; set; }
+}
---
+++ WpfApp1/Services/CoinGeckoService.cs
@@ -0,0 +1,36 @@
+﻿using System.Net.Http;
+using System.Text.Json;
+using CryptoViewer.Models;
+
+namespace CryptoViewer.Services;
+
+public class CoinGeckoService
+{
+    private readonly HttpClient _httpClient;
+
+    public CoinGeckoService()
+    {
+        _httpClient = new HttpClient
+        {
+            BaseAddress = new Uri("https://api.coingecko.com/api/v3/")
+        };
+    }
+
+    public async Task<List<Coin>> GetTopCoinsAsync(int limit = 10)
+    {
+        var url =$"coins/markets?vs_currency=usd&order=market_cap_desc&per_page={limit}&page=1&sparkline=false";
+
+        var response = await _httpClient.GetAsync(url);
+
+        response.EnsureSuccessStatusCode();
+
+        var json = await response.Content.ReadAsStringAsync();
+
+        var result = JsonSerializer.Deserialize<List<Coin>>(json, new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true
+        });
+
+        return result ?? new List<Coin>();
+    }
+}
